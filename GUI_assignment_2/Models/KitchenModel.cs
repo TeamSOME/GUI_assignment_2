c2ba13bef40d49570db5402f029429f4b6ea368c
Cecillia <201900001@post.au.dk>
2021-04-19 09:59:59 +0200

    MODELS TILFØJET (UDEN INDMAD)

---
+++ GUI_assignment_2/Models/KitchenModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GUI_assignment_2.Models
+{
+    public class KitchenModel
+    {
+    }
+}
---
+++ GUI_assignment_2/Models/ReceptionModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GUI_assignment_2.Models
+{
+    public class ReceptionModel
+    {
+        public int ReceptionId { get; set; }
+        //public ICollection<T> Ts { get; set; }
+    }
+}
