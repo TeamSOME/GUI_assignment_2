5d03b5b2dd28ab903d625ef7217a57c5fdb0618d
Cecillia <201900001@post.au.dk>
2021-04-22 15:14:41 +0200

    Controllers

--- GUI_assignment_2/Controllers/HomeController.cs
+++ GUI_assignment_2/Controllers/HomeController.cs
@@ -2,11 +2,14 @@
 using GUI_assignment_2.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+
 
 namespace GUI_assignment_2.Controllers
 {
@@ -22,15 +25,41 @@ namespace GUI_assignment_2.Controllers
             _db = db;
         }
 
+        /*PAGES*/
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Reception()
         {
             return View();
         }
+
+        public IActionResult Kitchen()
+        {
+            return View();
+        }
+
+        public IActionResult Restaurant()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+        /*ACTION RESULTS*/
+
+        //public async Task<IActionResult> Kitchen(string id)
+        //{
+            
+        //}
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
